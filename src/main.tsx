bdfa6087f8c035798d9438eb822c2754d2fea0cf
surprise2024-cpu <surprise2024@gmail.com>
2026-07-23 14:35:46 +0200

    alert css

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { AlertProvider } from './components/Alert/AlertProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <AlertProvider>
+      <BrowserRouter>
       <App />
     </BrowserRouter>
+    </AlertProvider>
   </StrictMode>,
 )
